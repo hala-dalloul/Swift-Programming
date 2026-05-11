7072a39189df2c88cfe802c6fd5a66d4f2b969e1
Hala M. Dalloul <haladalloul42@gmail.com>
2026-05-11 22:18:18 +0300

    Add variables for personal information and print them

---
+++ variables.swift
@@ -0,0 +1,13 @@
+
+var firstName = "Hala", middelName = "Monzer" , lastName = "Dalloul";
+
+print(firstName + " " + middelName + " " + lastName)
+
+var age : Int = 21;
+print(age)
+
+var isStudent = true
+print(isStudent)
+
+var gpa : Float = 3.4
+print(gpa)
